3d25f38f4c33d54aa09f2a547687fcdf81ec993e
13416 <zll@qq.com>
2022-09-03 15:47:46 +0800

    项目经理第一次将工程提交到本地仓库.

---
+++ git_001_javase0903/src/main/java/com/bjpowernode/Hello.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class Hello {
+    public static void main(String args){
+        System.out.println("这是项目经理开发的第一个类......");
+    }
+}
---
+++ git_002_javaweb0903/src/main/java/com/bjpowernode/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.bjpowernode.controller;
+
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("项目经理开发的web应用程序.......");
+    }
+}
