fa645d31db9e258a716f8e4547d16ebf88faf78e
13416 <zll@qq.com>
2022-09-03 20:08:18 +0800

    项目经理完成了Hello类中内容的修改

--- git_001_javase0903/src/main/java/com/bjpowernode/Hello.java
+++ git_001_javase0903/src/main/java/com/bjpowernode/Hello.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class Hello {
     public static void main(String args){
         System.out.println("这是项目经理开发的第一个类......");
+        System.out.println("项目经理进行更新处理......");
     }
 }
